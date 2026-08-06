534e79a81f0cb3ee1dcf558cea619c72470ded44
dangyulee <dg200101@gmail.com>
2026-08-06 09:09:54 +0900

    refactor(global): Swagger server URL 주입 제거
    - SwaggerConfig에서 swagger.server-url 주입 및 서버 항목 추가 로직 제거
    - forward-headers 기반으로 접속 호스트 자동 감지하도록 단순화

--- src/main/kotlin/com/meotsa/global/config/SwaggerConfig.kt
+++ src/main/kotlin/com/meotsa/global/config/SwaggerConfig.kt
@@ -5,34 +5,22 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
-import io.swagger.v3.oas.models.servers.Server
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class SwaggerConfig(
-    @Value("\${swagger.server-url:}") private val serverUrl: String,
-) {
+class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI {
         val securitySchemeName = "bearerAuth"
 
-        val openAPI =
-            OpenAPI()
-                .info(
-                    Info()
-                        .title("Meotsa Hackathon API")
-                        .description("멋사 해커톤 백엔드 API 문서")
-                        .version("v1.0.0"),
-                )
-
-        if (serverUrl.isNotBlank()) {
-            openAPI.addServersItem(Server().url(serverUrl))
-        }
-
-        return openAPI
-            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Meotsa Hackathon API")
+                    .description("멋사 해커톤 백엔드 API 문서")
+                    .version("v1.0.0"),
+            ).addSecurityItem(SecurityRequirement().addList(securitySchemeName))
             .components(
                 Components().addSecuritySchemes(
                     securitySchemeName,
